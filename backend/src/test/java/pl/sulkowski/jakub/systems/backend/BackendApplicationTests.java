9d9c449eb207dc81484ae3220fd6341e989e8b30
Nidan23 <dev.sulkowski@gmail.com>
2022-08-08 14:42:53 +0200

    [INIT] Initialized project backend

---
+++ backend/src/test/java/pl/sulkowski/jakub/systems/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sulkowski.jakub.systems.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
