36fd5fb7951f05e9b356c5bb04d2b143eec02c3c
Nidan23 <dev.sulkowski@gmail.com>
2022-08-08 23:03:32 +0200

    [#4][ADD] Added findby functions

--- backend/src/main/java/pl/sulkowski/jakub/systems/backend/repository/UserRepository.java
+++ backend/src/main/java/pl/sulkowski/jakub/systems/backend/repository/UserRepository.java
@@ -3,5 +3,10 @@ package pl.sulkowski.jakub.systems.backend.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.sulkowski.jakub.systems.backend.entity.UserEntity;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    List<UserEntity> findByNameContainingIgnoreCase(String name);
+    List<UserEntity> findBySurnameContainingIgnoreCase(String surname);
+    List<UserEntity> findByLoginContainingIgnoreCase(String login);
 }
