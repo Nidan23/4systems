14d21da48a83f6b211343c33916a9cf277e4f9b1
Nidan23 <dev.sulkowski@gmail.com>
2022-08-09 15:18:32 +0200

    [#5][ADD] Added user service template

---
+++ frontend/src/app/service/user.service.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+import {BackendConnectorService} from "./backend-connector.service";
+import {Observable} from "rxjs";
+import {UserModel} from "../model/user.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private backendConnectorService: BackendConnectorService) { }
+
+  getAllUsers(offset: number, limit: number, sortDirection: Sorting, sortColumnName: SortColumnName): Observable<UserModel[]> {
+    return this.backendConnectorService.getUsersFromServer(offset, limit, sortDirection, sortColumnName)
+  }
+
+  addUsersToDb(users: any, fileType: string): Observable<boolean> {
+    return this.backendConnectorService.sendUsersToServer(users, fileType)
+  }
+}
