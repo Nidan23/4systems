50ef76a32b0fdcff33fa13b078da09faa7f1acf6
Nidan23 <dev.sulkowski@gmail.com>
2022-08-09 13:09:04 +0200

    [#4][ADD] Added page components

---
+++ frontend/src/app/view/add-users-page/add-users-page.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-add-users-page',
+  templateUrl: './add-users-page.component.html',
+  styleUrls: ['./add-users-page.component.scss']
+})
+export class AddUsersPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
