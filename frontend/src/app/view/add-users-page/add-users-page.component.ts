3859fa490a305602574e23e0880de357d294768b
Nidan23 <dev.sulkowski@gmail.com>
2022-08-09 15:17:50 +0200

    [#5][ADD] Added file type & router change

--- frontend/src/app/view/add-users-page/add-users-page.component.ts
+++ frontend/src/app/view/add-users-page/add-users-page.component.ts
@@ -1,15 +1,24 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
+import {Page} from "../../utils/interface/page";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-add-users-page',
   templateUrl: './add-users-page.component.html',
   styleUrls: ['./add-users-page.component.scss']
 })
-export class AddUsersPageComponent implements OnInit {
+export class AddUsersPageComponent implements Page {
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
+  headerText: string = 'Here you can upload your users 2 DB'
+  acceptableFileType: string = '.json'
+
+  changeAcceptableFileType(fileType: string) {
+    this.acceptableFileType = fileType
   }
 
+  addUsers(event: any) {
+    this.router.navigateByUrl('/view-users')
+  }
 }
