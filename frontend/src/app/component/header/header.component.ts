52b2929e2a2bd4053d0c590d2b0d3c6566877a73
Nidan23 <dev.sulkowski@gmail.com>
2022-08-09 13:43:15 +0200

    [#4][ADD] Added header component

---
+++ frontend/src/app/component/header/header.component.ts
@@ -0,0 +1,13 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'header-component',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+
+  @Input()
+  headerText!: String;
+
+}
