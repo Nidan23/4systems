a075c180525d76025864aaf6d57509640321a9e1
Nidan23 <dev.sulkowski@gmail.com>
2022-08-10 11:11:39 +0200

    [#8][REFACTOR] Refactored run bat

--- run.bat
+++ run.bat
@@ -1,17 +1,9 @@
-docker stop _4systems__4systems-backend_1 _4systems__4systems-frontend_1
+docker stop 4systems-backend-1 4systems-frontend-1 4systems-postgres-1
 
-docker network rm _4systems_default
+docker network rm 4systems_default
 
-docker rm _4systems__4systems-backend_1 _4systems__4systems-frontend_1
+docker rm 4systems-backend-1 4systems-frontend-1 4systems-postgres-1
 
-docker image rm _4systems__4systems-backend _4systems__4systems-frontend
-
-cd backend
-./mvnw package
-
-cd ../frontend
-npm run build --prod
-
-cd ..
+docker image rm 4systems_backend 4systems_frontend postgres
 
 docker-compose up -d
